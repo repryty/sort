179282dabc8c96782cec0940066bb3e2b05a8b5a
REPRYTY_01 <62415785+repryty@users.noreply.github.com>
2022-06-04 15:26:54 +0900

    a

--- js/main.js
+++ js/main.js
@@ -6,11 +6,23 @@ function btn1(){
     document.getElementById("dummy1").innerHTML =  `정렬 방법: ${howtosort}<br>정렬 순서: ${target2.options[target2.selectedIndex].text}`;
 
 
-    document.getElementById("text2").value = bubble(text1);
+    if(target2.options[target2.selectedIndex].value=="up"&&target1.options[target1.selectedIndex].value=="bubble"){
+        document.getElementById("text2").value = bubble(text1);
+    } else if(target2.options[target2.selectedIndex].value=="down"&&target1.options[target1.selectedIndex].value=="bubble"){
+        document.getElementById("text2").value = (bubble(text1)).split(" ").reverse().join(" ");
+    } else if(target2.options[target2.selectedIndex].value=="up"&&target1.options[target1.selectedIndex].value=="selection"){
+        document.getElementById("text2").value = selection(text1)
+    } else if(target2.options[target2.selectedIndex].value=="down"&&target1.options[target1.selectedIndex].value=="selection"){
+        document.getElementById("text2").value = (selection(text1)).split(" ").reverse().join(" ");
+    } else if(target2.options[target2.selectedIndex].value=="up"&&target1.options[target1.selectedIndex].value=="insert"){
+        document.getElementById("text2").value = selection(text1)
+    } else if(target2.options[target2.selectedIndex].value=="down"&&target1.options[target1.selectedIndex].value=="insert"){
+        document.getElementById("text2").value = (selection(text1)).split(" ").reverse().join(" ");
+    }
     
 }
 
-function bubble(text, ud){
+function bubble(text){
     var data = [], temp;
     for(var i = text.split(" ").length; i>=0; i--){
         data[i]=text.split(" ")[i];
@@ -25,6 +37,27 @@ function bubble(text, ud){
         }
     }
     var result="";
+    for(var i = 0; i<text.split(" ").length; i++){
+        result=result+data[i]+" ";
+    }
+    return result;
+}
+
+function selection(text){
+    var data=text.split(" "), least, temp;
+    const how=text.split(" ").length;
+    for(var i=0; i<=2; i++){
+        least = i;
+        for(var j=i+1; j<=how; j++){
+            if(data[j]<data[least]){
+                least = j;
+            }
+        }
+        temp=data[i];
+        data[i]=data[least];
+        data[least]=temp;
+    }
+    result="";
     for(var i = 0; i<text.split(" ").length; i++){
         result=result+data[i]+" ";
     }
